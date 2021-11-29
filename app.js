cf55fe0bcdd9e7b5c2e386271970e2af217664d0
ajsarmah <thisisamlan49@gmail.com>
2021-11-29 15:43:05 +0530

    fixed error in calculateSnakePosition funnction

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var BOARD = document.getElementById("board");
 var BOARD_SIZE = 100;
 var start_btn = document.getElementById("start-game");
 var IS_PLAYING = false;
-var snakePosition = [2, 1];
+var snakePosition = [5, 4, 3, 2, 1];
 var foodPosition;
 var direction = "E";
-var nextHeadPosition = 3;
+var nextHeadPosition = 6;
+var GAME_SPEED = 500; //in milisecond
 // rendering the board on screen
 var generateBoard = function (boardSize) {
     for (var i = 0; i < boardSize; i++) {
@@ -17,12 +18,12 @@ generateBoard(BOARD_SIZE);
 //calculating game stuff
 function calculateSnakePosition(snakePosition) {
     var newSnakePosition = [];
+    console.log(nextHeadPosition);
     for (var i = 0; i < snakePosition.length; i++) {
         if (i == 0) {
-            var pos = snakePosition[i] - snakePosition[i + 1];
+            var pos = nextHeadPosition - snakePosition[i];
             newSnakePosition.push(nextHeadPosition);
             nextHeadPosition = nextHeadPosition + pos;
-            console.log(newSnakePosition, nextHeadPosition);
         }
         else {
             newSnakePosition.push(snakePosition[i - 1]);
@@ -54,9 +55,31 @@ function main() {
     if (IS_PLAYING) {
         setTimeout(function () {
             window.requestAnimationFrame(main);
-        }, 500);
+        }, GAME_SPEED);
     }
 }
+//User input
+function handleUserInput(event) {
+    var key = event.key.toUpperCase();
+    console.log(nextHeadPosition);
+    if (key === "W" && direction !== "N" && direction !== "S") {
+        direction = "N";
+        nextHeadPosition = snakePosition[0] - 10;
+    }
+    else if (key === "S" && direction !== "N" && direction !== "S") {
+        direction = "S";
+        nextHeadPosition = snakePosition[0] + 10;
+    }
+    else if (key === "A" && direction !== "W" && direction !== "E") {
+        direction = "W";
+        nextHeadPosition = snakePosition[0] - 1;
+    }
+    else if (key === "D" && direction !== "W" && direction !== "E") {
+        direction = "E";
+        nextHeadPosition = snakePosition[0] + 1;
+    }
+}
+window.addEventListener("keydown", handleUserInput);
 //initializing and stopping the game
 start_btn.addEventListener("click", function () {
     IS_PLAYING = !IS_PLAYING;
--- app.ts
+++ app.ts
@@ -3,10 +3,11 @@ const BOARD = document.getElementById("board") as HTMLDivElement;
 const BOARD_SIZE = 100;
 const start_btn = document.getElementById("start-game") as HTMLButtonElement;
 let IS_PLAYING = false;
-let snakePosition: number[] = [2, 1];
+let snakePosition: number[] = [5, 4, 3, 2, 1];
 let foodPosition: number;
 let direction = "E";
-let nextHeadPosition = 3;
+let nextHeadPosition = 6;
+const GAME_SPEED = 500; //in milisecond
 
 // rendering the board on screen
 const generateBoard = (boardSize: number) => {
@@ -20,12 +21,12 @@ generateBoard(BOARD_SIZE);
 //calculating game stuff
 function calculateSnakePosition(snakePosition: number[]) {
   let newSnakePosition: number[] = [];
+  console.log(nextHeadPosition);
   for (let i = 0; i < snakePosition.length; i++) {
     if (i == 0) {
-      const pos = snakePosition[i] - snakePosition[i + 1];
+      const pos = nextHeadPosition - snakePosition[i];
       newSnakePosition.push(nextHeadPosition);
       nextHeadPosition = nextHeadPosition + pos;
-      console.log(newSnakePosition, nextHeadPosition);
     } else {
       newSnakePosition.push(snakePosition[i - 1]);
     }
@@ -57,9 +58,29 @@ function main() {
   if (IS_PLAYING) {
     setTimeout(() => {
       window.requestAnimationFrame(main);
-    }, 500);
+    }, GAME_SPEED);
   }
 }
+
+//User input
+function handleUserInput(event: KeyboardEvent) {
+  let key = event.key.toUpperCase();
+  console.log(nextHeadPosition);
+  if (key === "W" && direction !== "N" && direction !== "S") {
+    direction = "N";
+    nextHeadPosition = snakePosition[0] - 10;
+  } else if (key === "S" && direction !== "N" && direction !== "S") {
+    direction = "S";
+    nextHeadPosition = snakePosition[0] + 10;
+  } else if (key === "A" && direction !== "W" && direction !== "E") {
+    direction = "W";
+    nextHeadPosition = snakePosition[0] - 1;
+  } else if (key === "D" && direction !== "W" && direction !== "E") {
+    direction = "E";
+    nextHeadPosition = snakePosition[0] + 1;
+  }
+}
+window.addEventListener("keydown", handleUserInput);
 
 //initializing and stopping the game
 start_btn.addEventListener("click", () => {
